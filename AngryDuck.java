0c93275bb5ba094d1ac94232455a16f0a62bda60
TongeAdrien <67654635+TongeAdrien@users.noreply.github.com>
2020-07-16 10:18:52 -0700

    Teleprter update 1

--- AngryDuck.java
+++ AngryDuck.java
@@ -1,17 +1,38 @@
 // Adrien Tonge T00245172
 
-public class AngryDuck extends WaterFowl 
+public class AngryDuck extends WaterFowl implements Teleporter
 {
-     public AngryDuck (String name, String colour)
+     private int duckX,duckY;
+     
+     public void teleport (int x, int y)
      {
-          super(name, colour);
+          if (x <=1000 && y<500 && x>0 && y >0)
+          {
+               duckY = y;
+               duckX = x;
+          }
+          else
+          {
+               System.out.println("Teleport failed");
+               
+          }
      }
-     public void swim()
+     public String getPosition()
      {
-          System.out.println(super.name + " is swimming");    
+          String position = (duckX+","+duckY);
+          return position;
      }
-     public void fly()
-     {
-          System.out.println(super.name + " is flying");    
-     }
+
+public AngryDuck (String name, String colour)
+{
+     super(name, colour);
+}
+public void swim()
+{
+     System.out.println(super.name + " is swimming");    
+}
+public void fly()
+{
+     System.out.println(super.name + " is flying");    
+}
 }
