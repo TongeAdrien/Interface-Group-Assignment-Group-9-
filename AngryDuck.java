2eb34c993c7cfe386391a661d409e488bb333f48
TongeAdrien <67654635+TongeAdrien@users.noreply.github.com>
2020-07-16 10:33:08 -0700

    Teleporter update

--- AngryDuck.java
+++ AngryDuck.java
@@ -1,8 +1,29 @@
 /*NGUYEN VU QUANG - T00643038*/
-public class AngryDuck extends WaterFowl
+public class AngryDuck extends WaterFowl implements Teleporter
 {
   public AngryDuck(String name, String primaryColor)
   {
     super(name,primaryColor);
   }
+
+ private int duckX,duckY;
+     
+     public void teleport (int x, int y)
+     {
+          if (x <=1000 && y<500 && x>0 && y >0)
+          {
+               duckY = y;
+               duckX = x;
+          }
+          else
+          {
+               System.out.println("Teleport failed");
+               
+          }
+     }
+     public String getPosition()
+     {
+          String position = (duckX+","+duckY);
+          return position;
+     }
 }
--- CuriousBunny.java
+++ CuriousBunny.java
@@ -1,5 +1,5 @@
 /*NGUYEN VU QUANG - T00643038*/
-public class CuriousBunny extends Animal implements AttackerInterface
+public class CuriousBunny extends Animal implements Teleporter
 {
   public CuriousBunny(String name, String primaryColor)
   {
@@ -64,4 +64,24 @@ public class CuriousBunny extends Animal implements AttackerInterface
   {
     return super.toString()+". The number of kills of " + primaryColor + " " +name+ ": "+getKillCount();
   }
+   private int bunnyX,bunnyY;
+     
+     public void teleport (int x, int y)
+     {
+          if (x <=1000 && y<1000 && x>0 && y >500)
+          {
+               bunnyY = y;
+               bunnyX = x;
+          }
+          else
+          {
+               System.out.println("Teleport failed");
+               
+          }
+     }
+     public String getPosition()
+     {
+          String position = (bunnyX+","+bunnyY);
+          return position;
+     }
 }
