588aafc03559d3e6fd8d47a111f9d2dd6af2cb58
Uyen[Brianna] Pham <brianna.pham799@gmail.com>
2020-07-16 12:43:47 -0700

    Update Teleporter.java

--- Teleporter.java
+++ Teleporter.java
@@ -1,7 +1,7 @@
 import java.awt.Point;
 public interface Teleporter
 {
-  Point locate = new Point(1000,1000);
+ Point locate = new Point(1000,1000); //Create X, Y coordinate with x:y = 1000:1000
  public void teleport(int x, int y);
  public String getPosition();
 }
