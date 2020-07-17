84c052fb12f77be6f9e68a95993dfee17ac0046c
TongeAdrien <67654635+TongeAdrien@users.noreply.github.com>
2020-07-17 15:20:43 -0700

    Add files via upload

---
+++ HappyPig.java
@@ -0,0 +1,32 @@
+/*NGUYEN VU QUANG - T00643038*/
+public class HappyPig extends Animal
+{
+  public HappyPig(String name, String primaryColor)
+  {
+    super(name,primaryColor);
+  }
+  
+  public void sleep()
+  {
+    if(health==0)
+    {
+      System.out.println(primaryColor+" "+name+" is dead!! It is in an eternal sleep. ");
+    }
+    else
+    {
+    System.out.println(primaryColor+" "+name+" is sleeping");
+    }
+  }
+  
+  public void roll()
+  {
+    if(health==0)
+    {
+      System.out.println(primaryColor+" "+name+" is dead!! It cannot fly.");
+    }
+    else
+    {
+    System.out.println(primaryColor+" "+name+" is rolling");
+    }
+  }
+}
